35b1a169d0f2f1548dd25bc8b314ecf6222d580c
sven.bartel90@gmail.com <sven.bartel90@gmail.com@63abd641-5a90-76d0-e791-76f985dc49d1>
2012-10-20 22:56:33 +0000

    initial directory structure,  autoclass loader, logging and dbmanager

---
+++ trunk/bwl-praktikum/bwl-praktikum/application/registry.php
@@ -0,0 +1,12 @@
+<?php 
+class Registry{
+    
+    public static $objects = array();
+    
+    public static $settings= array("config" => array(), 
+                                   "classpaths" => array(),
+                                   "database" => array()
+                                    );
+    
+}
+?>
