ae876e66600e6ebfbf0644af5fc93223500bf038
sven.bartel90 <sven.bartel90@63abd641-5a90-76d0-e791-76f985dc49d1>
2012-10-22 17:03:03 +0000

    noch viel zu tun...

---
+++ trunk/bwl-praktikum/webshop/application/classes/Request.class.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Sven
+ * Date: 22.10.12
+ * Time: 18:40
+ * To change this template use File | Settings | File Templates.
+ */
+class Request
+{
+    public $get, $post;
+
+    public static function val($get, $post)
+    {
+        $obj = new Request();
+        $obj->$get = $get;
+        $obj->$post = $post;
+
+        return $obj;
+    }
+}
---
+++ trunk/bwl-praktikum/webshop/application/view/classes/Default_Controller.view.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Sven
+ * Date: 22.10.12
+ * Time: 18:38
+ * To change this template use File | Settings | File Templates.
+ */
+class Default_Controller
+{
+
+}
