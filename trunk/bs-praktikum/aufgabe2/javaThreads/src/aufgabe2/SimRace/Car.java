a04d74e4140900eac4148e6c2452b243d52f77fb
breitenstein.alexander <breitenstein.alexander@63abd641-5a90-76d0-e791-76f985dc49d1>
2012-11-13 00:44:52 +0000

    bs-praktikum: java rennen ohne Unfall soweit implementiert, kann sein dass funktionen von Thread genutzt werden die nicht verwendet werden sollen

--- trunk/bs-praktikum/aufgabe2/javaThreads/src/aufgabe2/SimRace/Car.java
+++ trunk/bs-praktikum/aufgabe2/javaThreads/src/aufgabe2/SimRace/Car.java
@@ -1,6 +1,6 @@
 package aufgabe2.SimRace;
 
-import java.util.Random;
+import services.RandomManager;
 
 /**
  * Created with IntelliJ IDEA.
@@ -8,17 +8,50 @@ import java.util.Random;
  * Time: 22:31
  */
 public class Car implements Runnable {
+
+    public long id;
+    public long rounds;
+    long summedRoundTime;
+
+    private Car(long id, long rounds) {
+        this.id = id;
+        this.rounds = rounds;
+    }
+
+    public static Car create(long id) {
+        return new Car(id,1);
+    }
+
+    public static Car create(long id, long rounds) {
+        return new Car(id, rounds);
+    }
+
     @Override
     public void run() {
         Thread cThread = Thread.currentThread();
-        System.out.println("Hallo ich bin: " + cThread.getName());
-        long teeTimer = 2000L;
-        try {
-            Thread.currentThread().wait(teeTimer);
-            System.out.println(cThread + " hat " + teeTimer + " gewartet!");
-        } catch (InterruptedException e) {
-            System.out.println(cThread.getName() + " war zu ungeduldig und hat nicht gewartet!");
+        System.out.println("Hallo ich bin: " + cThread.getName() + ", jetzt bin ich Auto: " + this.id);
+        summedRoundTime = 0;
+
+        for (int i = 0; i < this.rounds; i++) {
+            long roundTime = RandomManager.longNumber(10000,2000);
+
+            try {
+                Thread.sleep(roundTime);
+//                System.out.println(cThread.getName() + "Auto :"+ this.id + " ist eine rundenZeit von" + roundTime + " in Runde: " + i + " gefahren!");
+                System.out.println("Auto = " + id + " Runde = " + (i+1) + " Zeit = " + roundTime);
+                summedRoundTime += roundTime;
+
+            } catch (InterruptedException e) {
+                System.out.println(cThread.getName() + " war zu ungeduldig und hat nicht gewartet!");
+            }
         }
-        System.out.println(cThread.getName() + " ist fertig!");
+
+//        System.out.println(cThread.getName() + " ist fertig!");
+//        System.out.println("Fertig: Auto = " + id + " mit einer StreckenZeit von: " + this.summedRoundTime);
+    }
+
+    @Override
+    public String toString() {
+        return "Auto: " + id + " Streckenzeit : " + this.summedRoundTime;
     }
 }
--- trunk/bs-praktikum/aufgabe2/javaThreads/src/aufgabe2/SimRace/SimRace.java
+++ trunk/bs-praktikum/aufgabe2/javaThreads/src/aufgabe2/SimRace/SimRace.java
@@ -1,6 +1,8 @@
 package aufgabe2.SimRace;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -13,9 +15,11 @@ public class SimRace {
     static List<Car> carList = new ArrayList<Car>();
     static List<Thread> threadList = new ArrayList<Thread>();
 
+    static long cars = 10, rounds = 5;
+
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-            carList.add(new Car());
+        for (int i = 0; i < cars; i++) {
+            carList.add(Car.create(i,rounds));
         }
 
         for(Car car: carList){
@@ -26,13 +30,24 @@ public class SimRace {
             t.start();
         }
 
+        //Join only needed, if Threads need to be finished before next process
         for(Thread t: threadList){
             try {
                 t.join();
             } catch (InterruptedException e) {
-                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                e.printStackTrace();
             }
         }
+
+        Comparator<Car> carComparator = new CarComparator();
+
+        Collections.sort(carList, carComparator);
+
+        System.out.println("Rangliste:");
+
+        for(Car car: carList)
+            System.out.println(car.toString());
+
     }
 
 }
