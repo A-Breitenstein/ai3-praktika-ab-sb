c9f625047bf3aef120b66b0ad68e3b6345c96b31
breitenstein.alexander@googlemail.com <breitenstein.alexander@googlemail.com@63abd641-5a90-76d0-e791-76f985dc49d1>
2012-11-13 21:26:22 +0000

    bs-praktikum: cleaned

--- trunk/bs-praktikum/aufgabe2/hawsh.c
+++ trunk/bs-praktikum/aufgabe2/hawsh.c
@@ -1,5 +1,5 @@
 // <hawsh, HAW-Shell. In Funktionalität stark eingeschraenkte Shell.>
-// <Breitenstein, Bartel> 
+// <Breitenstein, Bartel>
 // <11.11.2012>
 
 //includes
@@ -20,61 +20,43 @@ bool lastZeichenUnd(char[] &command);
 //MAIN
 int main(){
 	while (quit != true){
-
 		type_prompt();
 		read_command(&command);
 
 		if(command == "quit")){
-
 			quit = true;
-	
 		}else{
-
 			if(lastZeichenUnd(&command)){
-		
-	
 				PIDstatus = fork();
-			
+
 				if(PIDstatus < 0){
-			
-	
-					printf("Unable to fork");	
-	
+					printf("Unable to fork");
+
 					if(PIDstatus == 0){
-					
 						execve(command,0);
 					}
 				}
-	
 			}else{
-
 				if(PIDstatus > 0){
-			
 					wait(PIDstatus, &status, 0);
-
 				}else{
-
 					execve(command,0);
-
-				}		
+				}
 			}
+		}
 	}
-
-}
 
 	//letztes Zeichen bestimmen
 	bool lastZeichenUnd(char[] &command){
 		const int iLaenge = strlen(command);
-		
-		return (command[iLaenge] == '&' );	
+
+		return (command[iLaenge] == '&' );
 	}
 
 	void read_command(char[] &command){
 		fgets(command,31,stdin);
 	}
-	
+
 	void type_prompt(){
 		printf("prompt!");
 	}
-
-
