5b83a38c34d22d68b3d9b7e7b78e85022095cabd
breitenstein.alexander <breitenstein.alexander@63abd641-5a90-76d0-e791-76f985dc49d1>
2012-11-13 00:47:30 +0000

    bs-praktikum: services vergessen hinzuzufügen

---
+++ bs-praktikum/aufgabe2/javaThreads/Services/src/services/RandomManager.java
@@ -0,0 +1,37 @@
+package services;
+
+import java.util.Random;
+
+/**
+ * AD-Praktikum
+ * Team: 13
+ * Date: 03.10.12
+ * Time: 20:39
+ */
+public class RandomManager  {
+
+
+//    public static int[] initRandomArray(int arraySize, int upperBound, int lowerBound) {
+//        int array[] = new int[arraySize];
+//        Random random = new Random();
+//
+//        upperBound += (1 + Math.abs(lowerBound));
+//
+//        for(int i = 0; i < array.length; i++){
+//            array[i] = random.nextInt(upperBound)+lowerBound;
+//        }
+//
+//        return array;
+//    }
+
+    public static long longNumber(int upperBound, int lowerBound) {
+        long number;
+        Random random = new Random();
+
+        upperBound += (1 + Math.abs(lowerBound));
+
+        number = random.nextInt(upperBound)+lowerBound;
+
+        return number;
+    }
+}
