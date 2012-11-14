ac84a05c691a4a608af5e9deaa468427f5e05ee7
breitenstein.alexander <breitenstein.alexander@63abd641-5a90-76d0-e791-76f985dc49d1>
2012-11-14 17:16:55 +0000

    bs-praktikum: hf

--- bs-praktikum/aufgabe2/hawsh.c
+++ bs-praktikum/aufgabe2/hawsh.c
@@ -6,77 +6,109 @@
 //includes
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/types.h>
+#include <sys/wait.h> 
+
+#define FALSE 0
+#define TRUE 1
 #define ZERO 0
+#define EQUAL 0
 #define VERSION 0.01
 
-//DEFINITIONS
-char[31] command;
-bool quit = false;
+//DEKLARATIONS ggf. DEFINITIONS
+char command[100];
+char quit = 0;
+pid_t PIDstatus;
+int status;
+char *params[] = {NULL};
+
+char *quitString = "quit", *version = "version", *help = "help";
 
 //PROTOTYPES
 void read_command(char *command);
 void type_prompt();
-void printHelp();
-bool lastZeichenUnd(char *command);
-bool firstZeichenSlash(char *command);
+void print_help();
+void print_version();
+char lastZeichenUnd(char *command);
+char firstZeichenSlash(char *command);
 
 //MAIN
-int main(){
-	while (quit != true){
+int main(int argc, char **argv, char **envp){
+	while (quit == FALSE){
+ 
+                
 		type_prompt();
 		read_command(command);
-
-		if(command == "quit")){
-			quit = true;
-		}else if(command == "version"){
-			printf("version: %f",VERSION);
-		
-		}else if(firstZeichenSlash(command))
+                printf(getcwd(command,sizeof(command)));
+                
+		if(strcmp(command,quitString) == EQUAL){
+			quit = TRUE;
+		}else if(strcmp(command,version) == EQUAL){
+                    print_version();
+		}else if(strcmp(command,help) == EQUAL){
+			print_help();
+                        
+                                
+		//}else if(firstZeichenSlash(command) == EQUAL){
 			//changeDIR someHow
-		}else if(command == "help"){
-			printHelp();
+                    
 		}else{
 			if(lastZeichenUnd(command)){
 				PIDstatus = fork();
 
 				if(PIDstatus < 0){
-					printf("Unable to fork");
+				printf("Unable to fork");
 
 					if(PIDstatus == 0){
-						execve(command,0);
+						execve(command,params,0);
 					}
 				}
 			}else{
+                                PIDstatus = fork();
+                                printf("%d\n",PIDstatus);
 				if(PIDstatus > 0){
-					wait(PIDstatus, &status, 0);
+					//waitpid(PIDstatus, &status, 0);
 				}else{
-					execve(command,0);
+					execve("ls",params,envp);
+                                        
 				}
 			}
 		}
 	}
+}
 
 	//letztes Zeichen bestimmen
-	bool lastZeichenUnd(char *command){
-		const int iLaenge = strlen(command);
-
-		return (command[iLaenge-1] == '&' );
+	char lastZeichenUnd(char *command){
+            const int iLaenge = strlen(command);
+            
+            return (command[iLaenge-1] == '&' );
 	}
 	
-	bool firstZeichenSlash(char *command){
-		
-		return(command[0] == '/')
+	char firstZeichenSlash(char *command){
+            return (command[0] == '/');
 	}
 
 	void read_command(char *command){
-		fgets(command,31,stdin);
+		
+            fgets(command,100,stdin);
+            const int iLaenge = strlen(command);
+            command[iLaenge-1] = 0x0;
+
 	}
 
 	void type_prompt(){
-		printf("prompt!");
+            printf("prompt!>");
 	}
+        void print_version(){
+                printf("version: %.2f\n",VERSION);
+        }
 	
-	void printHelp(){
-		printf("Du bekommst nichts Hilfe!");
+	void print_help(){
+            printf("Du bekommst nichts Hilfe!\n");
 	}
