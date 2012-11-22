780df1ac87ff59c551f22560f4ae652f5a15a983
breitenstein.alexander@googlemail.com <breitenstein.alexander@googlemail.com@63abd641-5a90-76d0-e791-76f985dc49d1>
2012-11-22 01:14:58 +0000

    gka-praktikum: counter implemented, dijkstra scheint zu spinnen

--- trunk/gka-praktikum/test/aufgabe2/OptimalWayTest.java
+++ trunk/gka-praktikum/test/aufgabe2/OptimalWayTest.java
@@ -5,7 +5,6 @@ import aufgabe1.GKAFileManager;
 import org.junit.Test;
 import static aufgabe2.OptimalWay.*;
 
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -18,11 +17,14 @@ import java.util.Set;
 public class OptimalWayTest {
     @Test
     public void testDijkstra() throws Exception {
+        System.out.println("\n");
         AttributedGraph attriGraph = (AttributedGraph) GKAFileManager.importGraph("graph3_a2.gka");
         Map<String,CustomVertex> husumHamburg = OptimalWay.dijkstra(attriGraph, "Husum", "Hamburg");
+            System.out.println("dijkstra - Zugriffe auf dem Weg von Husum -> Hamburg: " + OptimalWay.dijkstraCounter);
         Map<String,CustomVertex> mindenHamburg = OptimalWay.dijkstra(attriGraph, "Minden", "Hamburg");
+            System.out.println("dijkstra - Zugriffe auf dem Weg von Minden -> Hamburg: " + OptimalWay.dijkstraCounter);
         Map<String,CustomVertex> muensterHamburg = OptimalWay.dijkstra(attriGraph, "Münster", "Hamburg");
-
+            System.out.println("dijkstra - Zugriffe auf dem Weg von Münster -> Hamburg: " + OptimalWay.dijkstraCounter);
 //        for (CustomVertex vertex : husumHamburg.values()) {
 //            System.out.println(vertex);
 //        }
@@ -35,19 +37,21 @@ public class OptimalWayTest {
 
     @Test
     public void testaaaaaStern(){
+        System.out.println("\n");
         AttributedGraph attriGraph = (AttributedGraph) GKAFileManager.importGraph("graph3_a2.gka");
         Map<String,CustomVertex> husumHamburg = OptimalWay.aaaaaaStern(attriGraph,"Husum","Hamburg");
+            System.out.println("aStern - Zugriffe auf dem Weg von Husum -> Hamburg: " + OptimalWay.aSternCounter);
         Map<String,CustomVertex> mindenHamburg = OptimalWay.aaaaaaStern(attriGraph, "Minden","Hamburg");
+            System.out.println("aStern - Zugriffe auf dem Weg von Minden -> Hamburg: " + OptimalWay.aSternCounter);
         Map<String,CustomVertex> muensterHamburg = OptimalWay.aaaaaaStern(attriGraph, "Münster","Hamburg");
-
+            System.out.println("aStern - Zugriffe auf dem Weg von Münster -> Hamburg: " + OptimalWay.aSternCounter);
 //        for (CustomVertex vertex : husumHamburg.values()) {
 //            System.out.println(vertex);
 //        }
         System.out.println("--- aaaaStern ---");
         getPathInSout("Husum","Hamburg",husumHamburg);
         getPathInSout("Minden","Hamburg",mindenHamburg);
-        Set<CustomVertex> list = getPathInSout("Münster","Hamburg",muensterHamburg);
-        System.out.println(list);
+        getPathInSout("Münster","Hamburg",muensterHamburg);
     }
 
 }
