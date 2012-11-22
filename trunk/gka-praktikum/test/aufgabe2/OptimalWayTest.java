cd2ee33400baef70556fbe12793d51a916c60e95
breitenstein.alexander <breitenstein.alexander@63abd641-5a90-76d0-e791-76f985dc49d1>
2012-11-22 14:08:01 +0000

    gka-praktikum: hotfixmix

--- trunk/gka-praktikum/test/aufgabe2/OptimalWayTest.java
+++ trunk/gka-praktikum/test/aufgabe2/OptimalWayTest.java
@@ -15,9 +15,11 @@ import java.util.Set;
  * Time: 18:23
  */
 public class OptimalWayTest {
+
     @Test
     public void testDijkstra() throws Exception {
         System.out.println("\n");
+        System.out.println("--- Dijkstra ---");
         AttributedGraph attriGraph = (AttributedGraph) GKAFileManager.importGraph("graph3_a2.gka");
         Map<String,CustomVertex> husumHamburg = OptimalWay.dijkstra(attriGraph, "Husum", "Hamburg");
             System.out.println("dijkstra - Zugriffe auf dem Weg von Husum -> Hamburg: " + OptimalWay.dijkstraCounter);
@@ -28,16 +30,22 @@ public class OptimalWayTest {
 //        for (CustomVertex vertex : husumHamburg.values()) {
 //            System.out.println(vertex);
 //        }
-        System.out.println("--- Dijkstra ---");
+
         getPathInSout("Husum","Hamburg",husumHamburg);
         getPathInSout("Minden","Hamburg",mindenHamburg);
         getPathInSout("Münster","Hamburg",muensterHamburg);
 
+        System.out.println("\n");
+        attriGraph = (AttributedGraph) GKAFileManager.importGraph("graph_alex.gka");
+        Map<String,CustomVertex> dublinHokkaido = OptimalWay.dijkstra(attriGraph, "Dublin", "Hokkaido");
+        System.out.println("dijkstra - Zugriffe auf dem Weg von Dublin -> Hokkaido: " + OptimalWay.dijkstraCounter);
+        getPathInSout("Dublin","Hokkaido",dublinHokkaido);
     }
 
     @Test
     public void testaaaaaStern(){
         System.out.println("\n");
+        System.out.println("--- aaaaStern ---");
         AttributedGraph attriGraph = (AttributedGraph) GKAFileManager.importGraph("graph3_a2.gka");
         Map<String,CustomVertex> husumHamburg = OptimalWay.aaaaaaStern(attriGraph,"Husum","Hamburg");
             System.out.println("aStern - Zugriffe auf dem Weg von Husum -> Hamburg: " + OptimalWay.aSternCounter);
@@ -48,10 +56,16 @@ public class OptimalWayTest {
 //        for (CustomVertex vertex : husumHamburg.values()) {
 //            System.out.println(vertex);
 //        }
-        System.out.println("--- aaaaStern ---");
+
         getPathInSout("Husum","Hamburg",husumHamburg);
         getPathInSout("Minden","Hamburg",mindenHamburg);
         getPathInSout("Münster","Hamburg",muensterHamburg);
+
+        System.out.println("\n");
+        attriGraph = (AttributedGraph) GKAFileManager.importGraph("graph_alex.gka");
+        Map<String,CustomVertex> dublinHokkaido = OptimalWay.aaaaaaStern(attriGraph, "Dublin", "Hokkaido");
+        System.out.println("aStern - Zugriffe auf dem Weg von Dublin -> Hokkaido: " + OptimalWay.aSternCounter);
+        getPathInSout("Dublin","Hokkaido",dublinHokkaido);
     }
 
 }
