f8d5b27212b742428be75ef3eae86de2da6cba50
sven.bartel90@gmail.com <sven.bartel90@gmail.com@63abd641-5a90-76d0-e791-76f985dc49d1>
2012-12-04 20:40:52 +0000

    bs-praktikum: problem mit den vordränglern gelöst :D

--- trunk/bs-praktikum/aufgabe3/semaphoreMonitore/src/semaphore_loesung_2/Student.java
+++ trunk/bs-praktikum/aufgabe3/semaphoreMonitore/src/semaphore_loesung_2/Student.java
@@ -1,5 +1,7 @@
 package semaphore_loesung_2;
 
+import java.util.Random;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Sven
@@ -9,30 +11,41 @@ package semaphore_loesung_2;
 public class Student implements Runnable {
     private String name;
     private Kasse zugewieseneKasse;
+    private Random rand = new Random(System.currentTimeMillis());
+    private static int MAX_BEZAHLZEIT = 1800;
+    private static int MAX_ESSENSZEIT = 2220;
 
     public Student(String name) {
         this.name = name;
     }
 
     public Student kasseZuweisen(Kasse kasse){
-        System.out.println(name + " stellt sich an Kasse "+kasse.name());
+        //System.out.println(name + " stellt sich an Kasse "+kasse.name());
         zugewieseneKasse = kasse;
-        kasse.warteschlangenlaengeErhoehen();
+        kasse.inWarteschlangeEinreihen(this);
         return this;
     }
     @Override
     public void run() {
         zugewieseneKasse.besetzten();
-        System.out.println(name+" ist jetz an Kasse "+zugewieseneKasse.name()+" dran, .....");
+        if(zugewieseneKasse.werIstDerNaechste() != this){
+            zugewieseneKasse.freigeben();
+            System.out.println(name+" hat versucht an Kasse "+zugewieseneKasse.name()+" sich vorzudrängeln, zur strafe neu anstellen! ------>>");
+            return;
+        }
+
+
+
+        //System.out.println(name+" ist jetz an Kasse "+zugewieseneKasse.name()+" dran, .....");
         try {
 
-            Thread.sleep(500);
-            System.out.println((name + " verlässt die Kasse " + zugewieseneKasse.name()));
+            Thread.sleep(rand.nextInt(MAX_BEZAHLZEIT));
+            //System.out.println((name + " verlässt die Kasse " + zugewieseneKasse.name()));
             zugewieseneKasse.freigeben();
 
-            System.out.println(name + " beginnt zu essen ....");
-            Thread.sleep(500);
-            System.out.println(name+" ist fertig mit essen ");
+            //System.out.println(name + " beginnt zu essen ....");
+            Thread.sleep(rand.nextInt(MAX_ESSENSZEIT));
+            //System.out.println(name+" ist fertig mit essen ");
 
         } catch (InterruptedException e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
