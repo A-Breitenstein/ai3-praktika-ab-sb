3889756b708ce388a34a56a109c664a1ace186e7
sven.bartel90 <sven.bartel90@gmail.com>
2012-12-26 17:33:27 +0000

    gka-praktikum: Hierholzer nochmal überarbeitet, verwendet jetz keine Tiefensuche mehr um einen kreis zu erzeugen Das der testFlueryEulerBig solange dauert liegt an der Tiefensuche, die erzeugen pfade für das gefixte Haus vom Nikolaus ( eulertour.gka) liefern beide richtige touren
    verwendete quellen: 
    http://de.wikipedia.org/wiki/Algorithmus_von_Hierholzer
    http://de.wikipedia.org/wiki/Eulerkreisproblem
     http://www-i1.informatik.rwth-aachen.de/~algorithmus/algo9.php  (haus vom nikolaus gefixed )

--- gka-praktikum/src/aufgabe3/GraphBuilder.java
+++ gka-praktikum/src/aufgabe3/GraphBuilder.java
@@ -1,8 +1,10 @@
 package aufgabe3;
 
+import org.jgraph.graph.DefaultEdge;
 import org.jgrapht.Graph;
 import org.jgrapht.Graphs;
 import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.Pseudograph;
 import org.jgrapht.graph.WeightedPseudograph;
 import services.RandomManager;
 
@@ -20,14 +22,15 @@ public class GraphBuilder {
     private static String vertexName = "vertex";
 
     private String sourceVertex,
-                   targetVertex;
+            targetVertex;
 
     private double upperBound = 0d,
-                   lowerBound = 0d;
+            lowerBound = 0d;
 
     private int numberOfVertices = 0, numberOfEdges = 0;
 
     private boolean multiEdges = false;
+    private boolean loopsAllowed = false;
 
     private GraphBuilder() {
     }
@@ -36,7 +39,7 @@ public class GraphBuilder {
         return new GraphBuilder();
     }
 
-    public GraphBuilder setGraphStructure(int numberOfVertices, int numberOfEdges){
+    public GraphBuilder setGraphStructure(int numberOfVertices, int numberOfEdges) {
         this.numberOfVertices = numberOfVertices;
         this.numberOfEdges = numberOfEdges;
 
@@ -56,9 +59,15 @@ public class GraphBuilder {
         return this;
     }
 
+    public GraphBuilder allowLoops() {
+        this.loopsAllowed = true;
+
+        return this;
+    }
+
     public Graph generate() {
 
-        if(this.numberOfVertices == 0)
+        if (this.numberOfVertices == 0)
             throw new IllegalArgumentException("The Number of Vertices is lower than 1, numberOfVertices: " + numberOfVertices);
 
 
@@ -80,24 +89,24 @@ public class GraphBuilder {
         while (generatedGraph.vertexSet().size() < generatedVerticesNames.size()) {
 
             for (int i = 1; i < generatedVerticesNames.size(); i++) {
-                sourceVertex = generatedVerticesNames.get(i-1);
+                sourceVertex = generatedVerticesNames.get(i - 1);
                 targetVertex = generatedVerticesNames.get(i);
                 edgeWeight = RandomManager.doubleNumber(upperBound, lowerBound);
 
                 Graphs.addEdgeWithVertices(generatedGraph, sourceVertex, targetVertex, edgeWeight);
 
-                addedEdgeSet.add(sourceVertex+targetVertex);
-                addedEdgeSet.add(targetVertex+sourceVertex);
+                addedEdgeSet.add(sourceVertex + targetVertex);
+                addedEdgeSet.add(targetVertex + sourceVertex);
             }
         }
 
         boolean edgeCheck, equalVertices;
 
         for (int i = generatedGraph.edgeSet().size(); i < numberOfEdges; i++) {
-            do{
+            do {
                 int
-                sourceVertexIndex = RandomManager.intNumber(numberOfVertices,ZERO),
-                targetVertexIndex = RandomManager.intNumber(numberOfVertices,ZERO);
+                        sourceVertexIndex = RandomManager.intNumber(numberOfVertices, ZERO),
+                        targetVertexIndex = RandomManager.intNumber(numberOfVertices, ZERO);
 
                 sourceVertex = generatedVerticesNames.get(sourceVertexIndex);
                 targetVertex = generatedVerticesNames.get(targetVertexIndex);
@@ -107,14 +116,13 @@ public class GraphBuilder {
                 if (multiEdges) {
                     edgeCheck = equalVertices;
                 } else {
-                    edgeCheck = equalVertices && (addedEdgeSet.contains(sourceVertex+targetVertex) || addedEdgeSet.contains(targetVertex+sourceVertex));
+                    edgeCheck = equalVertices && (addedEdgeSet.contains(sourceVertex + targetVertex) || addedEdgeSet.contains(targetVertex + sourceVertex));
                 }
-            }while(edgeCheck);
-
+            } while (edgeCheck);
 
 
             edgeWeight = RandomManager.doubleNumber(upperBound, lowerBound);
-            Graphs.addEdgeWithVertices(generatedGraph,sourceVertex,targetVertex,edgeWeight);
+            Graphs.addEdgeWithVertices(generatedGraph, sourceVertex, targetVertex, edgeWeight);
         }
 
         sout_graphGenerated(generatedGraph);
@@ -126,7 +134,7 @@ public class GraphBuilder {
         System.out.println("graph was generated with " + graph.vertexSet().size() + " Vertices and " + graph.edgeSet().size() + " Edges");
     }
 
-    private static void addAllVertices(Graph graph, Collection objects){
+    private static void addAllVertices(Graph graph, Collection objects) {
 
         for (Object object : objects) {
             graph.addVertex(object);
@@ -139,5 +147,172 @@ public class GraphBuilder {
 
     private static void sout_generateVertices(int numberOfVertices) {
         System.out.println(numberOfVertices + " vertices are being generated");
+    }
+
+    /**
+     *  Erzeugt einen zusammenhängenden ungerichteten Graphen mit geradem Knoten Grad
+     *
+     *  Dies wird dadurch erreicht, dass die Knoten zuerst wie eine liste zusammen gehangen werden, danach
+     *  wird der Anfangs Knoten mit dem Endknoten verbunden dadurch hat das Grundgerüst einen geraden Knotengrad,
+     *  nun werden die zufällig weitere Edges eingefügt. Dabei wird darauf geachtet ob Mehrfachkanten und Schlaufen
+     *  erlaubt sind oder nicht. Ist nun die Anzahl an einzufügenden Kanten erreicht wird überprüft wie viele von den
+     *  Knoten ungreade Knotengrade haben, diese ungeraden Knoten werden dann untereinander verbunden.
+     *
+     * hätte man noch schöner bauen können, aber der code von dir war schon fast fertig xD
+     *
+     * @return   zusammenhängenden ungerichteten Graphen mit geradem Knoten Grad
+     */
+    public Graph generateConnectedUndirectedGraphWithEvenVertexGrade() {
+        Graph generatedGraph = new Pseudograph(DefaultEdge.class);
+
+        List<String> generatedVerticesNames = new ArrayList<String>();
+
+        Set<String> addedEdgeSet = new HashSet<String>();
+
+
+        sout_generateVertices(numberOfVertices);
+        for (int i = 0; i < numberOfVertices; i++) {
+            generatedVerticesNames.add(vertexName + i);
+        }
+
+
+        sout_addVertices(numberOfVertices);
+        // Graph zusammenhängend machen
+        while (generatedGraph.vertexSet().size() < generatedVerticesNames.size()) {
+
+            for (int i = 1; i < generatedVerticesNames.size(); i++) {
+                sourceVertex = generatedVerticesNames.get(i - 1);
+                targetVertex = generatedVerticesNames.get(i);
+
+                Graphs.addEdgeWithVertices(generatedGraph, sourceVertex, targetVertex);
+
+                addedEdgeSet.add(sourceVertex + targetVertex);
+                addedEdgeSet.add(targetVertex + sourceVertex);
+            }
+        }
+        // Zudiesem Zeitpunkt sollten alle Knoten bis auf den Start und End Knoten
+        // einen geraden Grad haben nun werden abschließen noch der Start und End Knoten verbunden
+        // damit diese auch einen geraden Grad bebekommen
+        sourceVertex = generatedVerticesNames.get(0);
+        targetVertex = generatedVerticesNames.get(generatedVerticesNames.size() - 1);
+
+        Graphs.addEdgeWithVertices(generatedGraph, sourceVertex, targetVertex);
+
+        addedEdgeSet.add(sourceVertex + targetVertex);
+        addedEdgeSet.add(targetVertex + sourceVertex);
+
+
+        // restliche edges einfügen
+        boolean equalVertices, edgeCheck;
+        int sourceVertexIndex, targetVertexIndex;
+
+        for (int i = generatedGraph.edgeSet().size(); i < numberOfEdges; i++) {
+            do {
+
+                sourceVertexIndex = RandomManager.intNumber(numberOfVertices, ZERO);
+                targetVertexIndex = RandomManager.intNumber(numberOfVertices, ZERO);
+
+                sourceVertex = generatedVerticesNames.get(sourceVertexIndex);
+                targetVertex = generatedVerticesNames.get(targetVertexIndex);
+
+
+                equalVertices = sourceVertex.equals(targetVertex);
+                if (multiEdges) {
+                    if (loopsAllowed)
+                        edgeCheck = false;
+                    else
+                        edgeCheck = equalVertices;
+                } else {
+                    if (loopsAllowed)
+                        edgeCheck = (addedEdgeSet.contains(sourceVertex + targetVertex) || addedEdgeSet.contains(targetVertex + sourceVertex));
+                    else
+                        edgeCheck = equalVertices || (addedEdgeSet.contains(sourceVertex + targetVertex) || addedEdgeSet.contains(targetVertex + sourceVertex));
+
+                }
+            } while (edgeCheck);
+
+            Graphs.addEdgeWithVertices(generatedGraph, sourceVertex, targetVertex);
+
+            addedEdgeSet.add(sourceVertex + targetVertex);
+            addedEdgeSet.add(targetVertex + sourceVertex);
+        }
+
+
+        // letzte phase, anpassen der restlichen knotengrade
+        List<String> oddVertecies = getVerteciesWithOddVertexGrad(generatedGraph);
+        int oddVerteciesCount = oddVertecies.size();
+        System.out.println("Found " + oddVerteciesCount + " Vertecies with odd grade");
+        if (oddVerteciesCount > 0) {
+            if (oddVerteciesCount % 2 == 0) {
+                while(oddVerteciesCount > 0) {
+                    do {
+
+                        sourceVertexIndex = RandomManager.intNumber(oddVerteciesCount, ZERO);
+                        targetVertexIndex = RandomManager.intNumber(oddVerteciesCount, ZERO);
+
+                        sourceVertex = oddVertecies.get(sourceVertexIndex);
+                        targetVertex = oddVertecies.get(targetVertexIndex);
+
+
+                        equalVertices = sourceVertex.equals(targetVertex);
+                        if (multiEdges) {
+                            if (loopsAllowed)
+                                edgeCheck = false;
+                            else
+                                edgeCheck = equalVertices;
+                        } else {
+                            if (loopsAllowed)
+                                edgeCheck = (addedEdgeSet.contains(sourceVertex + targetVertex) || addedEdgeSet.contains(targetVertex + sourceVertex));
+                            else {
+                                edgeCheck = equalVertices || (addedEdgeSet.contains(sourceVertex + targetVertex) || addedEdgeSet.contains(targetVertex + sourceVertex));
+                                // no loops fix
+                                if (oddVerteciesCount == 2 && !equalVertices) {
+                                    Graphs.addEdgeWithVertices(generatedGraph, sourceVertex, "noLoopsFixVertex");
+                                    Graphs.addEdgeWithVertices(generatedGraph, targetVertex, "noLoopsFixVertex");
+
+                                    System.out.println("had to fix graph, added noLoopsFixVertex to the graph to ensure the even vertex grade");
+                                    return generatedGraph;
+                                }
+                            }
+
+                        }
+                    } while (edgeCheck);
+
+                    Graphs.addEdgeWithVertices(generatedGraph, sourceVertex, targetVertex);
+
+                    addedEdgeSet.add(sourceVertex + targetVertex);
+                    addedEdgeSet.add(targetVertex + sourceVertex);
+
+                    oddVertecies = getVerteciesWithOddVertexGrad(generatedGraph);
+                    oddVerteciesCount = oddVertecies.size();
+                }
+            } else {
+                throw new IllegalArgumentException("fail... try again");
+            }
+        }
+
+        System.out.println(
+                (getVerteciesWithOddVertexGrad(generatedGraph).size() == 0) ?
+                        ("Alle Knoten besitzen einen geraden Grad ") :
+                        ("Es gibt einen Knoten der einen ungeraden Grad hat ... versuchs nochmal"
+                        ));
+        return generatedGraph;
+
+
+    }
+
+    /**
+     * <b>Hilfsfunktion</b> zu generateConnectedUndirectedGraphWithEvenVertexGrade(...) <br>
+     *  Bestimmt die Knoten mit den ungeraden Knotengrad in einem übergebenen Graphen
+     * @param g
+     * @return liste von Strings mit den Knoten Namen
+     */
+    private List<String> getVerteciesWithOddVertexGrad(Graph g) {
+        List<String> oddVertecies = new ArrayList<String>();
+        for (String vertex : (Collection<String>) g.vertexSet()) {
+            if (g.edgesOf(vertex).size() % 2 == 1)
+                oddVertecies.add(vertex);
+        }
+        return oddVertecies;
     }
 }
