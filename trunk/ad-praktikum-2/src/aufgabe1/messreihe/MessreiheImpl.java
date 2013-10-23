1d18577263d6861197472f17f5def0c39464a8df
breitenstein.alexander@googlemail.com <breitenstein.alexander@googlemail.com@63abd641-5a90-76d0-e791-76f985dc49d1>
2013-10-23 16:49:30 +0000

    imnport project

---
+++ trunk/ad-praktikum-2/src/aufgabe1/messreihe/MessreiheImpl.java
@@ -0,0 +1,38 @@
+package aufgabe1.messreihe;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Alexander Breitenstein
+ * Date: 21.10.13
+ * Time: 16:07
+ */
+public class MessreiheImpl implements Messreihe {
+
+    private float summedMesswerte;
+
+    private List<Float> listeVonMesswerte;
+
+
+
+    @Override
+    public void addMesswert(float messwert) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void addAllMesswert(float[] messwerte) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public float calculateMittelwert() {
+        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public float calculateVarianz() {
+        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
